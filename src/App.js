2eae5b009e94f22e3e10cb551032759a29c52e11
RafaelaMirabile <rafaelamirabile@gmail.com>
2023-10-25 12:57:12 -0300

    feat: layout of Navbar component completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from "react";
+import { Navbar, Footer } from "./components";
 import './App.css';
+import { Layout } from "antd";
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+
+        </Layout>
+      </div>
+      <div className="footer">
+        <Footer />
+      </div>
     </div>
   );
 }
 
-export default App;
+
