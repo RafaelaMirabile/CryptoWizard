817baf5032c17dd9a098dc0244692a31447ea269
RafaelaMirabile <rafaelamirabile@gmail.com>
2023-10-26 18:56:22 -0300

    feat: cryptoApi endpoint: /coins implemented

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { cryptoApi } from '../services/cryptoApi'
+import { setupListeners } from "@reduxjs/toolkit/query/react";
 
 export default configureStore({
-    reducer: {},
-})
+    reducer: {
+        [cryptoApi.reducerPath]: cryptoApi.reducer,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(cryptoApi.middleware),
+});
+
+
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,15 +1,26 @@
-/*const options = {
-    method: 'GET',
-    url: 'https://coinranking1.p.rapidapi.com/exchanges',
-    params: {
-      referenceCurrencyUuid: 'yhjMzLPhuIDl',
-      limit: '50',
-      offset: '0',
-      orderBy: '24hVolume',
-      orderDirection: 'desc'
-    },
-    headers: {
-      'X-RapidAPI-Key': '7da9cf8b36msh4527bf1039e2a71p17e7cfjsn2105e5092342',
-      'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
-    }
-  };*/
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeadres = {
+    'X-RapidAPI-Key': '7da9cf8b36msh4527bf1039e2a71p17e7cfjsn2105e5092342',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+}
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = (url) => ({
+    url,
+    headers: cryptoApiHeadres
+})
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+});
+
+export const {
+    useGetCryptosQuery
+} = cryptoApi;
